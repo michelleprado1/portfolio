e5157d7951bd6ef752393efb2f2cff281d4d2b87
michelleprado1 <ssab7.raymund@gmail.com>
2024-05-22 21:48:03 +0800

    portfolio

---
+++ script1.js
@@ -0,0 +1,11 @@
+// Follow me on Twitter & GitHub
+// https://twitter.com/sethdavis512
+// https://github.com/sethdavis512
+
+$(".slideshow").slick({
+    infinite: true,
+    autoplay: false,
+    dots: true,
+    arrows: true,
+    autoplaySpeed: 4000
+});
